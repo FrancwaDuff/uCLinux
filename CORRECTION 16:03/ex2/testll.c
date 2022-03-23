cfc42e251ceed1ab1e717338c1284e446c7e463a
FrancwaDfr <francois.dufour.study@gmail.com>
2022-03-23 12:02:10 -0400

    MAJ

--- CORRECTION 16:03/ex2/testll.c
+++ CORRECTION 16:03/ex2/testll.c
@@ -1,20 +1,41 @@
 /* test de la fonction lire ligne  */
 #include <stdio.h>
+#include <string.h>
+#include <signal.h>
 #include "es.h"
 
 #define LBUF 200
 char buf[LBUF];
+int RUN=1;
+
+/* Fonction qui va être appelé au moment de la reception du signal. */
+
+void interrupt(int S)
+{
+	switch(S){
+	case SIGINT :
+		RUN=0;
+		break;
+	default :
+		fprintf(stderr, "Recu signal %d ????\n", S);
+	}
+
+}
+
 
 int main(int N, char *P[])
 {
 	int n;
-	while (1){
-		printf("Taper un texte :\n");
+	signal(SIGINT, interrupt);
+	while (RUN){
+		printf("Taper un texte ou FIN pour stopper le programme :\n");
 		if ((n = lireligne(0, (void *)buf, LBUF, '\n')) < 0)
 			printf("Erreur dans la saisie de la ligne!");
-		else printf("%d caractères: <%s>\n", n, buf);
+		else
+			if(strcmp(buf, "FIN") == 0) break;
+			printf("%d caractères: <%s>\n", n, buf);
 	}
-	printf("Fin du programme");
+	printf("Fin du programme.\n");
 	return 0;
 
 }
